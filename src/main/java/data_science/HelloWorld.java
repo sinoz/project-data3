91d5e5f5571143c4f1fd940a1e9d2c8a3ecab2a3
Sino <ilyas.alfan.b@gmail.com>
2017-04-02 22:39:00 +0200

    Initial maven commit including maven plugins and junit dependency

---
+++ src/main/java/data_science/HelloWorld.java
@@ -0,0 +1,14 @@
+package data_science;
+
+/**
+ * A sample 'hello world' application.
+ * @author I.A
+ */
+public final class HelloWorld {
+  /**
+   * The main entry point to this sample application.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/TestHelloWorld.java
@@ -0,0 +1,13 @@
+import data_science.HelloWorld;
+import org.junit.Test;
+
+/**
+ * A JUnit test application to test the {@link HelloWorld} program.
+ * @author I.A
+ */
+public final class TestHelloWorld {
+  @Test
+  public void testHelloWorld() {
+    HelloWorld.main(null);
+  }
+}
