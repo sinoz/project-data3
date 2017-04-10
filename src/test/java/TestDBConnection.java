a6bfb520c03ccd2a164d5e308b4662411d358b87
Sino <ilyas.alfan.b@gmail.com>
2017-04-10 13:45:19 +0200

    Added Postgres HikariCP connection setup test

---
+++ src/test/java/TestDBConnection.java
@@ -0,0 +1,34 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+/**
+ * A test Hikari based database connection setup.
+ * @author I.A
+ */
+public final class TestDBConnection {
+  /**
+   * Establishes and tests the database connection.
+   */
+  @Test
+  public void testHikariDbConnection() throws Exception {
+    Class.forName("org.postgresql.Driver");
+
+    HikariConfig config = new HikariConfig();
+
+    config.setJdbcUrl("jdbc:postgresql://localhost:5432/bicycles");
+    config.setUsername("postgres");
+    config.setPassword("root");
+
+    config.addDataSourceProperty("cachePrepStmts", "true");
+    config.addDataSourceProperty("prepStmtCacheSize", "" + (1 << 8));
+
+    HikariDataSource dataSource = new HikariDataSource(config);
+    Connection connection = dataSource.getConnection();
+
+    Assert.assertNotNull(connection);
+  }
+}
