1fcefd4527dd8491350c561795d298ac2dfa1200
Sino <ilyas.alfan.b@gmail.com>
2017-04-19 10:54:14 +0200

    Added categorized theft count chart

--- src/main/java/data_science/ui/graph/GraphViewPane.java
+++ src/main/java/data_science/ui/graph/GraphViewPane.java
@@ -1,74 +1,45 @@
 package data_science.ui.graph;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Side;
-import javafx.scene.chart.PieChart;
+import data_science.ui.graph.pie.CategorizedTheftsGraph;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
+import java.util.Arrays;
+
 /**
  * A {@link BorderPane} that displays all of the data graphs.
- *
  * @author I.A
+ * @author Johan Bastinck
  */
 public final class GraphViewPane extends BorderPane {
 	/**
 	 * Creates a new {@link GraphViewPane}.
 	 */
 	GraphViewPane() {
-		TreeItem<String> rootItem = new TreeItem<String>("Inbox", null);
+		TreeItem<String> rootItem = new TreeItem<>("Inbox", null);
 		rootItem.setExpanded(true);
-		for (int i = 1; i < 6; i++) {
-			TreeItem<String> item = new TreeItem<String>("Message" + i);
-			rootItem.getChildren().add(item);
-		}
-		TreeView<String> tree = new TreeView<String>(rootItem);
+		rootItem.getChildren().addAll(Arrays.asList(new TreeItem<>("Categorized Theft Counts"), new TreeItem<>("Some other bs")));
 
-		tree.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+		TreeView<String> tree = new TreeView<>(rootItem);
 
-			@Override
-			public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+		tree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			switch (newValue.getValue()) {
+				case "Categorized Theft Counts":
+					presentCategorizedTheftsGraph();
 
-				TreeItem<String> selectedItem = (TreeItem<String>) newValue;
-				System.out.println("Selected Text : " + selectedItem.getValue());
-				if (selectedItem.getValue().equals("Message1")) {
-					ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-							new PieChart.Data("Diefstallen zonder geweld - " + 60 + "%", 60),
-							new PieChart.Data("Diefstallen met geweld - " + 40 + "%", 40));
-
-					PieChart pieChart = new PieChart(pieChartData);
-					pieChart.setLegendSide(Side.BOTTOM);
-					pieChart.setTitle("Fietsdiefstallen");
-					pieChart.setClockwise(true);
-					pieChart.setLabelsVisible(true);
-					pieChart.setLabelLineLength(40);
-
-					VBox vbox = new VBox(pieChart);
-					setCenter(vbox);
-				} else if (selectedItem.getValue().equals("Message2")) {
-					ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-							new PieChart.Data("Diefstallen zonder geweld - " + 50 + "%", 50),
-							new PieChart.Data("Diefstallen met geweld - " + 50 + "%", 50));
-
-					PieChart pieChart = new PieChart(pieChartData);
-					pieChart.setLegendSide(Side.BOTTOM);
-					pieChart.setTitle("Fietsdiefstallen");
-					pieChart.setClockwise(true);
-					pieChart.setLabelsVisible(true);
-					pieChart.setLabelLineLength(40);
-
-					VBox vbox = new VBox(pieChart);
-					setCenter(vbox);
-				}
+					break;
 			}
-
 		});
-		setLeft(tree);
 
+		setLeft(tree);
+	}
+
+	/**
+	 * TODO
+	 */
+	private void presentCategorizedTheftsGraph() {
+		setCenter(new VBox(new CategorizedTheftsGraph()));
 	}
 }
---
+++ src/main/java/data_science/ui/graph/pie/CategorizedTheftsGraph.java
@@ -0,0 +1,66 @@
+package data_science.ui.graph.pie;
+
+import data_science.database.query.graph.CategorizedTheftCounts;
+import data_science.model.TheftCategory;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+/**
+ * A {@link PieChart} to present a graph that shows the theft counts
+ * for each category.
+ * @author I.A
+ */
+public final class CategorizedTheftsGraph extends PieChart {
+	/**
+	 * Creates a new {@link CategorizedTheftsGraph}.
+	 */
+	public CategorizedTheftsGraph() {
+		configureGraph();
+		fillData();
+	}
+
+	private void configureGraph() {
+		setLegendSide(Side.BOTTOM);
+		setTitle("Aantal fietsdiefstallen per categorie");
+		setClockwise(true);
+		setLabelsVisible(true);
+		setLabelLineLength(40);
+	}
+
+	/**
+	 * Fills data inside of the graph by computing it and transforming it to a suitable presentable format.
+	 */
+	private void fillData() {
+		CategorizedTheftCounts.compute() // initiates a stream of data of categorized thef counts
+				.toList() // blocks until all data has been computed to turn it into a list of theft categories
+				.map(this::theftCategoriesToPieChartData) // transform the list to a suitable presentation format (PieChart.Data)
+				.subscribe((ObservableList<Data> data) -> Platform.runLater(() -> setData(data))); // then finally present it
+	}
+
+	/**
+	 * Transforms the given {@link List} of {@link TheftCategory}s to a suitable {@link ObservableList}
+	 * format that can be presented inside of this graph.
+	 */
+	private ObservableList<Data> theftCategoriesToPieChartData(List<TheftCategory> theftCategories) {
+		ObservableList<Data> data = FXCollections.observableArrayList();
+
+		// we utilize a functional data stream here to sum up the counts of each category to obtain a total of theft counts
+		double total = theftCategories.stream().mapToInt(TheftCategory::getCount).sum();
+
+		// and here we add data imperatively to the ObservableList for the sake of readability
+		DecimalFormat df = new DecimalFormat("#.##");
+		for (TheftCategory category : theftCategories) {
+			double percentage = ((double) category.getCount() / total) * 100D;
+
+			data.add(new Data(category.getName() + " - " + df.format(percentage) + "%", percentage));
+		}
+
+		return data;
+	}
+}
